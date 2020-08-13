c4445236aeb7b87b83534488db97224d72f23f52
Yue Wu <wuyuejl97@gmail.com>
2020-08-12 23:41:26 -0700

    Set up mongodb

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,29 @@
 import express, { Express } from 'express'
+import { MongoClient, Collection } from 'mongodb'
 import * as path from 'path'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT || 3001
 
 app.use(express.static(path.resolve(__dirname, 'client')))
 
-app.get('/api/hello', (_, res) => res.send('Hello world!'))
+app.get('/api/hello', async (_, res) => {
+    try {
+        await client.connect()
+        const stories: Collection = client.db('exteractive').collection('stories')
+
+        const result = await stories.findOne({})
+        res.send(result.text)
+    } finally {
+        await client.close()
+    }
+})
+
 app.get('*', (_, res) => res.sendFile(path.resolve(__dirname, 'index.html')))
+
+const client: MongoClient = new MongoClient(process.env.DB_URI)
 
 app.listen(port, () => console.log(`Listening on port ${port}...`))
