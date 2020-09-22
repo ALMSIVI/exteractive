724a3a9b5a91391b11a643c94412d658100e0b48
Yue Wu <wuyuejl97@gmail.com>
2020-09-21 21:31:19 -0700

    Server: fixed tests

--- server/test/stories.get.test.ts
+++ server/test/stories.get.test.ts
@@ -9,8 +9,9 @@ describe('Getting stories', () => {
 
     test('Root story can be retrieved', async () => {
         try {
-            const root = await StoriesDAO.getRoot()
-            expect(root.title).toBe('The Beginning')
+            const dbRes = await StoriesDAO.getRoot()
+            expect(dbRes.success).toBe(true)
+            expect(dbRes.data.title).toBe('The Beginning')
         } catch (e) {
             expect(e).toBeNull()
         }
@@ -18,9 +19,29 @@ describe('Getting stories', () => {
 
     test('Story can be retrieved', async () => {
         try {
-            const story = await StoriesDAO.getStoryById('5f3603b0b6372b04780a6b4a')
-            expect(story.title).toBe('Fantasy')
-            expect(story.parent).toEqual(new ObjectId('5f34de2b3ab21d58636a1103'))
+            const dbRes = await StoriesDAO.getStoryById('5f3603b0b6372b04780a6b4a')
+            expect(dbRes.success).toBe(true)
+            expect(dbRes.data.title).toBe('Fantasy')
+            expect(dbRes.data.parent).toEqual(new ObjectId('5f34de2b3ab21d58636a1103'))
+        } catch (e) {
+            expect(e).toBeNull()
+        }
+    })
+
+    test('Story not found', async () => {
+        try {
+            const dbRes = await StoriesDAO.getStoryById('5f34de2b3ab21d58636a1102')
+            expect(dbRes.success).toBe(true)
+            expect(dbRes.data).toBeNull()
+        } catch (e) {
+            expect(e).toBeNull()
+        }
+    })
+
+    test('Invalid story ID', async () => {
+        try {
+            const dbRes = await StoriesDAO.getStoryById('0000')
+            expect(dbRes.success).toBe(false)
         } catch (e) {
             expect(e).toBeNull()
         }
@@ -28,12 +49,14 @@ describe('Getting stories', () => {
 
     test('Children and tree can be retrieved', async () => {
         try {
-            const story = await StoriesDAO.getChildren('5f3603b0b6372b04780a6b4a')
-            expect(story[0].title).toBe('Country')
-            const tree = await StoriesDAO.getTree(story[0]._id.toHexString())
-            expect(tree.length).toBe(2)
-            expect(tree[0].title).toBe('The Beginning')
-            expect(tree[1].title).toBe('Fantasy')
+            let dbRes = await StoriesDAO.getChildren('5f3603b0b6372b04780a6b4a')
+            expect(dbRes.success).toBe(true)
+            expect(dbRes.data[0].title).toBe('Country')
+            dbRes = await StoriesDAO.getTree(dbRes.data[0]._id.toHexString())
+            expect(dbRes.success).toBe(true)
+            expect(dbRes.data).toHaveLength(2)
+            expect(dbRes.data[0].title).toBe('The Beginning')
+            expect(dbRes.data[1].title).toBe('Fantasy')
         } catch (e) {
             expect(e).toBeNull()
         }
@@ -41,9 +64,10 @@ describe('Getting stories', () => {
 
     test('Recent stories can be retrieved', async () => {
         try {
-            const recents = await StoriesDAO.getRecent()
-            expect(recents.length).toBeLessThanOrEqual(10)
-            const actualDates = recents.map(s => s.date)
+            const dbRes = await StoriesDAO.getRecent()
+            expect(dbRes.success).toBe(true)
+            expect(dbRes.data.length).toBeLessThanOrEqual(10)
+            const actualDates = dbRes.data.map(s => s.date)
             const expectedDates = actualDates.slice().sort((d1, d2) => d2.getTime() - d1.getTime())
             expect(actualDates).toEqual(expectedDates)
         } catch (e) {
